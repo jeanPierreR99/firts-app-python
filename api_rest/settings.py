f532039f97dbd11c3808f20ffccce3981c795bcf
jean pierre <kreyesf@unamad.edu.pe>
2023-05-28 19:11:09 -0500

    add postgre

--- api_rest/settings.py
+++ api_rest/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'api_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': '2L6WAfrPGpTN',
+        'HOST': 'ep-wispy-snow-489468-pooler.us-east-2.postgres.vercel-storage.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
