17b4a61575fe59dcb791b3587958323af9333753
jean pierre <kreyesf@unamad.edu.pe>
2023-05-28 20:02:53 -0500

    update v4

--- api_rest/settings.py
+++ api_rest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0az#rndpla81z+h=)@=p9m#0+*$bdj+)bgsh-2dey5awni_m_^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
